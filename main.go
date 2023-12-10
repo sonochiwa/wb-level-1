2406a32450ace61d76b86369254a57e1e584b95a
Рашид Мамаримов <sonochiwaxd@gmail.com>
2023-12-10 21:17:48 +0600

    Exercise 2

--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"wb-level-1/exercise_2"
-)
+import "wb-level-1/exercise_3"
 
 func main() {
 	//exercise_1.Run()
-	exercise_2.Run()
-	//exercise_3.Run()
+	//exercise_2.Run()
+	exercise_3.Run()
 }
