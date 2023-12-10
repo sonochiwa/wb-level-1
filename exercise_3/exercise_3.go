49b7b9b1fa60fde2c1785305dec096d3187ea5a4
Рашид Мамаримов <sonochiwaxd@gmail.com>
2023-12-10 19:06:24 +0600

    Exercise 3

--- exercise_3/exercise_3.go
+++ exercise_3/exercise_3.go
@@ -18,16 +18,10 @@ func Run() {
 	numbers := []int{2, 4, 6, 8, 10}
 	ch := make(chan int)
 
-	// Сумма квадратов числел
-	sum := 0
-
 	for _, num := range numbers {
 		go square(num, ch)
-
-		// Увеличиваем значение sum на квадрат числа из канала
-		sum += <-ch
+		fmt.Println(<-ch)
 	}
 
 	close(ch)
-	fmt.Println(sum)
 }
