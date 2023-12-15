f2e6ade394f09f05308fc26021b6abda2ffe3fd6
Рашид Мамаримов <sonochiwaxd@gmail.com>
2023-12-15 13:53:25 +0600

    Exercise 12

--- exercise_12/exercise_12.go
+++ exercise_12/exercise_12.go
@@ -1,8 +1,24 @@
 package exercise_12
 
+import "fmt"
+
 // Имеется последовательность строк - (cat, cat, dog, cat, tree) создать для нее
 // собственное множество.
 
-func Run() {
+func set(words []string) map[string]struct{} {
+	set := make(map[string]struct{})
 
+	// Преобразуем слайс в множество
+	for _, word := range words {
+		set[word] = struct{}{}
+	}
+
+	return set
+}
+
+func Run() {
+	words := []string{"cat", "cat", "dog", "cat", "tree"}
+	result := set(words)
+
+	fmt.Println(result)
 }
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"wb-level-1/exercise_11"
+	"wb-level-1/exercise_12"
 )
 
 func main() {
@@ -13,7 +13,8 @@ func main() {
 	//exercise_6.Run()
 	//exercise_8.Run()
 	//exercise_10.Run()
-	exercise_11.Run()
+	//exercise_11.Run()
+	exercise_12.Run()
 	//exercise_13.Run()
 	//exercise_16.Run()
 	//exercise_17.Run()
