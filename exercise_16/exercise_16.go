0a3b20157db9844e2e63caedf69e061966f85e61
Рашид Мамаримов <sonochiwaxd@gmail.com>
2023-12-15 10:45:36 +0600

    Exercise 16

--- exercise_16/exercise_16.go
+++ exercise_16/exercise_16.go
@@ -1,8 +1,22 @@
 package exercise_16
 
+import (
+	"fmt"
+	"sort"
+)
+
 // Реализовать быструю сортировку массива (quicksort) встроенными методами
 // языка.
 
-func Run() {
+// Функция сортировки
+func quickSort(a *[]int) {
+	sort.Slice(*a, func(i, j int) bool {
+		return (*a)[i] < (*a)[j]
+	})
+}
 
+func Run() {
+	a := []int{9, 7, 5, 11, 12, 2, 14, 3, 10, 6}
+	quickSort(&a)
+	fmt.Println(a) // [2 3 5 6 7 9 10 11 12 14]
 }
